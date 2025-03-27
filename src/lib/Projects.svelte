312ced89019b8a6b66f2dffe2356a2719b297488
Soham Deo <116385470+devSoham3@users.noreply.github.com>
2025-03-27 00:16:43 -0400

    Add Projects component to fetch and display project data

---
+++ src/lib/Projects.svelte
@@ -0,0 +1,29 @@
+<script>
+import {
+    onMount
+} from 'svelte';
+let projectData = null;
+let error = null;
+
+onMount(async () => {
+    try {
+        const res = await fetch('/data/projects.json');
+        projectData = await res.json();
+    } catch (err) {
+        error = err;
+    }
+});
+</script>
+
+<section id="about" class="mb-16 p-6 bg-gray-100 rounded-lg">
+    {#if error}
+    <p>Error loading data.</p>
+    {:else}
+    {#if projectData}
+    <!-- Projects -->
+    <div class="mb-16 p-6 bg-gray-200 rounded-lg">
+        <h2 class="text-3xl font-bold mb-6 text-center">Projects</h2>
+    </div>
+    {/if}
+    {/if}
+</section>
