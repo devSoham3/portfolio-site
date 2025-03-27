b51ed44193a1ffbf7de3fe48e283b89b31dbb4ff
Soham Deo <116385470+devSoham3@users.noreply.github.com>
2025-03-27 00:16:36 -0400

    Add Highlights component to display fetched highlight data

---
+++ src/lib/Highlights.svelte
@@ -0,0 +1,29 @@
+<script>
+    import {
+        onMount
+    } from 'svelte';
+    let highlightData = null;
+    let error = null;
+    
+    onMount(async () => {
+        try {
+            const res = await fetch('/data/highlights.json');
+            highlightData = await res.json();
+        } catch (err) {
+            error = err;
+        }
+    });
+    </script>
+
+<section id= "cv" class="mb-16 p-6 bg-gray-100 rounded-lg">
+    {#if error}
+    <p>Error loading data.</p>
+    {:else}
+    {#if highlightData}
+    <!-- Highlights -->
+    <div class="mb-16 p-6 bg-gray-200 rounded-lg">
+        <h2 class="text-3xl font-bold mb-6 text-center">Highlights</h2>
+    </div>
+    {/if}
+    {/if}
+</section>
