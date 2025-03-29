d99e8dc9deb009bd24aa5a594f23880ecfeef950
Soham Deo <116385470+devSoham3@users.noreply.github.com>
2025-03-29 01:25:29 -0400

    Enhance Highlights section with updated styling, improved layout, and added skeleton loader

--- src/lib/Highlights.svelte
+++ src/lib/Highlights.svelte
@@ -21,15 +21,15 @@ onMount(async () => {
     {:else}
     {#if highlightData}
     <!-- Highlights Section -->
-    <div class="mb-16 p-6 bg-surface rounded-lg border border-border">
-        <h2 class="text-3xl font-bold mb-6 text-center text-accent">Highlights</h2>
+    <div class="mb-16 p-6 bg-surface rounded-4xl border-4 border-rblue">
+        <h2 class="text-2xl font-prsst mb-6 text-center text-ryllw">Highlights</h2>
         <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
             {#each highlightData.skills as skill}
-            <div class="p-6 bg-background rounded-lg border border-border shadow">
-                <h4 class="font-semibold text-primary-light mb-2">{skill.category}</h4>
-                <div class="p-2 bg-surface rounded-lg">
+            <div class="p-6 bg-background rounded-3xl border-2 border-rblue shadow">
+                <h4 class="text-xl font-semibold font-retrogm text-rpink mb-2">{skill.category}</h4>
+                <div class="bg-surface rounded-lg">
                     {#each skill.items as item}
-                    <p class="text-foreground">{item}</p>
+                    <p class="text-foreground font-semibold text-lg font-pixelopt">{item}</p>
                     {/each}
                 </div>
             </div>
@@ -38,6 +38,22 @@ onMount(async () => {
     </div>
     {:else}
     <!-- TODO: write the skeleton -->
+    <!-- outer box -->
+    <div class="mb-16 p-6 bg-surface rounded-4xl border-4 border-rblue">
+        <div class="h-10 bg-ryllw rounded w-60 ml-80 mb-5 self-center"></div>
+        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+            <div class="p-6 bg-background rounded-3xl border-2 border-rblue shadow">
+                <div class="h-10 bg-rpink rounded w-45 mb-5 self-center"></div>
+            </div>
+            <div class="p-6 bg-background rounded-3xl border-2 border-rblue shadow">
+                <div class="h-10 bg-rpink rounded w-45 mb-5 self-center"></div>
+            </div>
+            <div class="p-6 bg-background rounded-3xl border-2 border-rblue shadow">
+                <div class="h-10 bg-rpink rounded w-45 mb-5 self-center"></div>
+            </div>
+            
+        </div>
+    </div>
     {/if}
     {/if}
 </section>
