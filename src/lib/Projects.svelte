ff0e27c257f9208d963ef06d1c27167f8d506d65
Soham Deo <116385470+devSoham3@users.noreply.github.com>
2025-03-29 20:13:30 -0400

    Revamp Projects section in Projects.svelte to bring layout, styling and project details in line with the rest of the page

--- src/lib/Projects.svelte
+++ src/lib/Projects.svelte
@@ -15,14 +15,43 @@ onMount(async () => {
 });
 </script>
 
-<section id="about" class="mb-16 p-6 bg-gray-100 rounded-lg">
+<section id="projects" class="mb-16 p-6 bg-background rounded-lg">
     {#if error}
     <p>Error loading data.</p>
     {:else}
     {#if projectData}
     <!-- Projects -->
-    <div class="mb-16 p-6 bg-gray-200 rounded-lg">
-        <h2 class="text-3xl font-bold mb-6 text-center">Projects</h2>
+    <div class="mb-16 p-6 bg-surface rounded-4xl border-4 border-rblue">
+        <h2 class="text-3xl font-prsst text-ryllw font-bold mb-6 text-center">Projects</h2>
+        <div class="grid grid-cols-1 sm:grid-cols-2 gap-4">
+            {#each projectData.projects as project}
+            <div class="p-6 bg-background rounded-3xl border-2 border-rblue shadow">
+                <h4 class="text-xl font-semibold font-retrogm text-rpink mb-2">{project.name}</h4>
+                <div class="bg-surface rounded-lg">
+                    {#each project.description as desc}
+                    <p class="text-foreground font-semibold text-lg font-pixelopt">{desc}</p>
+                    {/each}
+                    
+                    <h4 class="mt-4 text-md font-semibold font-retrogm text-rpink mb-2">Skillset</h4>
+                    {#each project.skillset as skill}
+                    <p class="text-rorng font-semibold text-md font-pixelopt">- {skill}</p>
+                    {/each}
+
+                    <h4 class="mt-4 text-md font-semibold font-retrogm text-rpink mb-2">Objectives</h4>
+                    {#each project.objectives as objv}
+                    <p class="text-foreground font-semibold text-md font-pixelopt">- {objv}</p>
+                    {/each}
+                    
+                    {#if project.results.length > 0}
+                    <h4 class="mt-4 text-md font-semibold font-retrogm text-rpink mb-2">Results</h4>
+                    {#each project.results as res}
+                    <p class="text-foreground font-semibold text-md font-pixelopt">- {res}</p>
+                    {/each}
+                    {/if}
+                </div>
+            </div>
+            {/each}
+        </div>
     </div>
     {/if}
     {/if}
