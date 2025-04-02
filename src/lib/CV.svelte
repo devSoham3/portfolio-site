a7dfa8e63b2b35e76218f9b51cf6d40133cf2d41
Soham Deo <116385470+devSoham3@users.noreply.github.com>
2025-04-02 00:12:18 -0400

    Refactor CV section layout and enhance styling to match the rest of the site

--- src/lib/CV.svelte
+++ src/lib/CV.svelte
@@ -15,25 +15,79 @@ onMount(async () => {
 });
 </script>
 
-<section id= "cv" class="mb-16 p-6 bg-gray-100 rounded-lg">
+<section id= "cv" class="mb-16 p-6 bg-surface rounded-lg text-foreground min-h-screen">
     {#if error}
     <p>Error loading data.</p>
     {:else}
     {#if cvData}
     <!-- Experience -->
-    <div class="mb-16 p-6 bg-gray-200 rounded-lg">
-        <h2 class="text-3xl font-bold mb-6 text-center">Experience</h2>
-    </div>
+    <div class="mb-16 p-6 bg-surface rounded-4xl border-4 border-rblue">
+        <h2 class="text-2xl font-prsst mb-6 text-center text-ryllw">Curriculum Vitae</h2>
+
+        <h3 class="text-xl font-retrogm mb-6 text-center text-rorng">Experience</h3>
+        <!-- Work -->
+        <div class="mb-16 p-6 bg-surface rounded-4xl border-2 border-rblue">
+            {#each cvData.experience.work as expr}
+            <h4 class="text-2xl font-retrogm">{expr.role} @ {expr.organization}</h4>
+            <p class="font-pixelopt">{expr.duration}, {expr.location}</p>
+            {#each expr.responsibilities as resp}
+            <p class="font-pixelopt mb-4 text-justify">- {resp}</p>
+            {/each}
+            {/each}
+        </div>
+        <!-- Research -->
+        <div class="mb-16 p-6 bg-surface rounded-4xl border-2 border-rblue">
+            {#each cvData.experience.research as expr}
+            <h4 class="text-2xl font-retrogm">{expr.role} @ {expr.organization}</h4>
+            <p class="font-pixelopt">{expr.duration}, {expr.location}</p>
+            {#if Array.isArray(expr.responsibilities)}
+                {#each expr.responsibilities as resp}
+                <p class="font-pixelopt mb-4 text-justify">- {resp}</p>
+                {/each}
+            {:else}
+                <p class="font-pixelopt mb-4 text-justify">- {expr.responsibilities}</p>
+            {/if}
+            {/each}
+        </div>
+        <!-- Teaching -->
+        <div class="mb-16 p-6 bg-surface rounded-4xl border-2 border-rblue">
+            {#each cvData.experience.teaching as expr}
+            <h4 class="text-2xl font-retrogm">{expr.role} @ {expr.organization}</h4>
+            <p class="font-pixelopt">{expr.duration}, {expr.location}</p>
+            {#if Array.isArray(expr.responsibilities)}
+                {#each expr.responsibilities as resp}
+                <p class="font-pixelopt mb-4 text-justify">- {resp}</p>
+                {/each}
+            {:else}
+                <p class="font-pixelopt mb-4 text-justify">- {expr.responsibilities}</p>
+            {/if}
+            {/each}
+        </div>
     <!-- Education -->
-    <div class="mb-16 p-6 bg-gray-200 rounded-lg">
-        <h2 class="text-3xl font-bold mb-6 text-center">Education</h2>
+    <div class="mb-16 p-6 bg-surface rounded-4xl border-2 border-rblue">
+        <h3 class="text-2xl font-bold mb-6 text-center">Education</h3>
         <div class="text-center md:text-left">
             {#each cvData.education as edu}
-            <h3 class="text-2xl font-semibold">{edu.institution}</h3>
-            <p>{edu.degree} ({edu.duration})</p>
-            <p>GPA: {edu.gpa}</p>
-            <p class="text-gray-700 mb-4 text-justify">Coursework: {edu.coursework.join(', ')}</p>
+            <h4 class="text-2xl font-retrogm">{edu.institution}</h4>
+            <p class="font-pixelopt mb-4 text-justify">{edu.degree} ({edu.duration})</p>
+            <p class="font-pixelopt mb-4 text-justify">GPA: {edu.gpa}</p>
+            <p class="font-pixelopt mb-4 text-justify">Coursework: {edu.coursework.join(', ')}</p>
             {/each}
+        </div>
+    </div>
+    </div>
+
+    
+    {:else}
+    <!-- Skeleton Loader / Placeholder Content -->
+    <div class="animate-pulse flex space-x-4">
+        <div class="rounded-full bg-gray-300 h-48 w-48"></div>
+        <div class="flex-1 space-y-4 py-1">
+            <div class="h-4 bg-gray-300 rounded w-3/4"></div>
+            <div class="space-y-2">
+                <div class="h-4 bg-gray-300 rounded"></div>
+                <div class="h-4 bg-gray-300 rounded w-5/6"></div>
+            </div>
         </div>
     </div>
     {/if}
